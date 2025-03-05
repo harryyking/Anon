f0694f79964e39e037cfb645f6be61246d8520ba
harryyking <arthurharry06@gmail.com>
2025-03-05 14:32:03 +0000

    new theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="bumblebee">
+    <html lang="en" data-theme="synthwave">
       <body
         className={brandFont.className}
       >
--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
-    <nav className="navbar bg-base-100 shadow-sm">
-    <div className="flex-1">
-    <a className="text-xl">Anon</a>
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="container flex h-14 items-center">
+      <div className="mr-4 flex">
+        <Link href="/" className="mr-6 flex items-center space-x-2">
+          <span className="font-bold text-xl">Reflect</span>
+        </Link>
+      </div>
+      <div className="flex flex-1 items-center justify-end space-x-2">
+        <button className='btn btn-secondary'>Log in</button>
+      </div>
     </div>
-
-    <div className="flex-none">
-     <button className="btn btn-secondary">Get Started</button>
-    </div>
-  </nav>
+  </header>
   )
 }
 
