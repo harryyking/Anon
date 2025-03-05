5b11ab5c13c2241ccb52e8983302e59eabbb371e
harryyking <arthurharry06@gmail.com>
2025-03-05 19:07:02 +0000

    new pges

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='bg-base-100 space-y-2 flex justify-center items-center'>
         <RateInput/>
         <AnonForm/>
     </div>
