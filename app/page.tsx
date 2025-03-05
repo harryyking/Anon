ebd4811d6ae85e424cb43b6e5fb549149d1bad09
harryyking <arthurharry06@gmail.com>
2025-03-05 10:24:48 +0000

    new sec

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <section className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
   
-          <div className="max-w-lg text-primary">
+          <div className="max-w-lg text-secondary">
             <h1 className="text-5xl font-bold ">Box Office News!</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
