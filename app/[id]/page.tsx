13512b27836d186ecb45ae82768f82ecb4052989
harryyking <arthurharry06@gmail.com>
2025-03-05 23:32:09 +0000

    nw id

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import AnonForm from '@/components/anon-form'
+import RateInput from '@/components/rate-input'
 
 const page = () => {
   return (
-    <div className='min-h-screen bg-base-100'>page</div>
+    <div className='min-h-screen bg-base-100'>
+       <RateInput/>
+       <AnonForm/>
+    </div>
   )
 }
 
