8efbacaf0df5e47264658db8ee2474ef2895a9c3
harryyking <arthurharry06@gmail.com>
2025-03-05 10:42:41 +0000

    sec cont

--- app/page.tsx
+++ app/page.tsx
@@ -17,13 +17,13 @@ export default function Home() {
       <section className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
   
-          <div className="max-w-lg text-secondary">
+          <div className="max-w-lg text-secondary-content">
             <h1 className="text-5xl font-bold ">Box Office News!</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
               quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-secondary">Get Started</button>
           </div>
         </div>
       </section>
