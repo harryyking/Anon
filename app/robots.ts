4ded92705c151e6b0ad04c254e95e71cf1865da0
harryyking <arthurharry06@gmail.com>
2025-03-12 13:32:49 +0000

    add changes

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: ['/'],
+      disallow: '/auth'
     },
     sitemap: `${baseUrl}/sitemap.xml`,
   }
